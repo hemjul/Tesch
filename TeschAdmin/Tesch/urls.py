f64b2606a134ae167fbebb4234c9c4a392cac45b
Hemanth Redy Julakanti <hemanthreddy.julakanti@gmail.com>
2018-09-08 12:05:28 +0530

    Tesch

--- TeschAdmin/Tesch/urls.py
+++ TeschAdmin/Tesch/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'Tesch'
 urlpatterns=[
     path('',views.login,name='login'),
-    path('login/error?',views.home, name='home')
+    path('home',views.home, name='home')
 ]
